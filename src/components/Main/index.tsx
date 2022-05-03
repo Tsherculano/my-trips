dd878354bbabc0e186363f38d4682265fa1de2ee
Tsherculano <tico.herculano@gmail.com>
2022-05-03 05:55:46 -0300

    feat: Add Dynamic import and include map

--- src/components/Main/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Main() {
-  return (
-    <div>
-      <h1>NextJS na Prática</h1>
-      <a href="#">Link</a>
-    </div>
-  );
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06093B" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+          integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+          crossOrigin=""
+        />
         <meta
           name="description"
           content="A simple project toshow my favorite spts in the world."

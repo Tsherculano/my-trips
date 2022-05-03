5596f3c932128622cc31bc3d000a87bff826cb3f
Tsherculano <tico.herculano@gmail.com>
2022-05-03 05:32:04 -0300

    feat: Initial config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,14 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06093B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project toshow my favorite spts in the world."
         />
       </Head>
       <GlobalStyles />

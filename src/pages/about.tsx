a44cb5d2bd5f27e3507429932ae2d8ed706401db
Tsherculano <tico.herculano@gmail.com>
2022-05-04 06:28:14 -0300

    feat: Config .env and create pages folder with client.ts and queries.ts

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,21 @@
+import client from 'graphql/client';
+import { GET_PAGES } from 'graphql/queries';
 import AboutTemplate from '../../templates/About';
 
-export default function About() {
+//getStaticPaths => gerar as urls em build time /about /trip/natal
+//getStaticProps => buscar dados da página (props) - build time (estático)
+//getServerSideProps => buscar dados da página (props) - runtime - toda requisição (bundle fica no server)
+//getInitalProps => buscar dados da página (props) - runtime - toda requisição (bundle também vai para o client) Caso queira fazer lógicas no client side - hydrate
+
+export default function AboutPage() {
   return <AboutTemplate />;
 }
+
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES);
+  console.log(pages);
+
+  return {
+    props: {},
+  };
+};
